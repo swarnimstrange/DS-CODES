1ea0cc64d9e072ba7a9c31a123464413e07c6ad9
Swarnim Rai <62171065+swarnimstrange@users.noreply.github.com>
2023-05-24 08:31:15 +0530

    Update AdditionServiceImpl.java

--- DS ASSIGN/D1/AdditionServiceImpl.java
+++ DS ASSIGN/D1/AdditionServiceImpl.java
@@ -1,16 +1,24 @@
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class AdditionServiceImpl extends UnicastRemoteObject implements AdditionService
-{
-    protected AdditionServiceImpl() throws RemoteException
-    {
+public class AddServer extends UnicastRemoteObject implements AddServerIntf {
+    public AddServer() throws RemoteException {
         super();
     }
 
-    @Override
-    public int addNumbers(int num1, int num2) throws RemoteException
-    {
-        return num1+num2;
+    public double add(double a, double b) throws RemoteException {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        try {
+            AddServer server = new AddServer();
+            Naming.rebind("AddServer", server);
+            System.out.println("Server started.");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
     }
 }
