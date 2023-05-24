a2c8bd149a8d6111380ab18a3a73dd02de275dcc
Swarnim Rai <62171065+swarnimstrange@users.noreply.github.com>
2023-05-24 08:28:38 +0530

    Update Server.java

--- DS ASSIGN/D1/Server.java
+++ DS ASSIGN/D1/Server.java
@@ -1,16 +1,50 @@
-import java.rmi.registry.Registry;
-import java.rmi.registry.LocateRegistry;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class Server {
+public class AddServer extends UnicastRemoteObject implements AddServerIntf {
+    public AddServer() throws RemoteException {
+        super();
+    }
+
+    public double add(double a, double b) throws RemoteException {
+        return a + b;
+    }
+
     public static void main(String[] args) {
         try {
-            AdditionService additionService = new AdditionServiceImpl();
-            Registry registry = LocateRegistry.createRegistry(1099);
-            registry.rebind("AdditionService", additionService);
-            System.out.println("Server is ready...");
+            AddServer server = new AddServer();
+            Naming.rebind("AddServer", server);
+            System.out.println("Server started.");
+
+            // Start a new thread for each client request
+            while (true) {
+                new ClientHandler(server).start();
+            }
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
+
+class ClientHandler extends Thread {
+    private AddServerIntf server;
+
+    public ClientHandler(AddServerIntf server) {
+        this.server = server;
+    }
+
+    public void run() {
+        try {
+            // Process the client request
+            // In this case, we are adding 10 and 20
+            double result = server.add(10, 20);
+
+            // Display the intermediate sum calculated by each thread
+            System.out.println("Intermediate sum: " + result);
+        } catch (Exception e) {
+            System.err.println("Exception in client handler: " + e.toString());
             e.printStackTrace();
         }
     }
